c7e106a1c67217d8f6c9ffb777d65ddbcf0b3056
davidmubernal <davidmubernal@gmail.com>
2024-07-14 21:08:11 +0200

    [Add] Mongo Conexion
    Update the API to manage the movies data from the Mongo DB and validate input and output

--- src/api.py
+++ src/api.py
@@ -3,24 +3,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+from mongo import MongoManager
 
 class Movie(BaseModel):
-    _id:int
+    id: Optional[str] = None
+    title:str
+    director:str
+    year:int = Field(min=1845)
+    score:int = Field(min=1, max=5)
+
+class MovieUpdate(BaseModel):
     title:str
     director:str
     year:int
     score:int
 
-movies = [
-    {
-        "_id": "1",
-        "title":"Holita",
-        "director":"Cuenting Tarantino",
-        "year":"2025",
-        "score":"4"
-    }
-    ]
 origins = [
     "http://localhost:5173"
 ]
@@ -30,49 +30,32 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"], #TODO: limit this in function of api
+    allow_methods=["GET","POST","DELETE","PUT"],
     allow_headers=["*"]
                    )
 
 @app.get("/movies")
-def get_movies():
-    return movies
+def get_movies() -> List[Movie]:
+    return MongoManager.find_movies()
 
 @app.get("/movies/")
-def get_movie(title:str=None, director:str=None, year:int=None, score:int=None):
-    dic = []
-    for movie in movies:
-        if title and title not in movie['title']:
-            continue
-        if director and director not in movie['director']:
-            continue
-        if year and year != int(movie['year']):
-            continue
-        if score and score != int(movie['score']):
-            continue
-        dic.append(movie)
-    return dic
+def get_movie(title:str=None, director:str=None, year:int=None, score:int=None) -> List[Movie]:
+    movies = []
+    for movie in MongoManager.find_movies(title=title, director=director, year=year, score=score):
+        _movie = movie
+        _movie['id'] = str(movie['_id'])
+        del _movie['_id']
+        movies.append(_movie)
+    return movies
 
-@app.put("/movies/{id}")
-def put_movie(id:int, film: Movie):
-    for movie in movies:
-        if int(movie["_id"]) != id:
-            continue
-        movie["title"] = film.title
-        movie["director"] = film.director
-        movie["year"] = film.year
-        movie["score"] = film.score
+@app.put("/movies/{_id}")
+def put_movie(_id:str, film: MovieUpdate) -> bool:
+    return MongoManager.update_movie(_id, film.title, film.director, film.year, film.score)
 
 @app.post("/movies/")
-def post_movie(film:Movie):
-    film_json = film.model_dump()
-    film_json["_id"] = len(movies)+1
-    movies.append(film_json)
+def post_movie(film:Movie) -> bool:
+    return MongoManager.insert_movie(film.title, film.director, film.year, film.score)
 
-@app.delete("/movies/{id}")
-def delete_movie(id:int):
-    for movie in movies:
-        if int(movie["_id"]) != id:
-            continue
-        movies.remove(movie)
-        return
+@app.delete("/movies/{_id}")
+def delete_movie(_id:str) -> bool:
+    return MongoManager.delete_movie(_id)
